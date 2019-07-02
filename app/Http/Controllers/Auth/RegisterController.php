c2a067e2f417c827f8af29713e66aedf7e5a2e7b
amitbauriya <amit001bauriya@gmail.com>
2019-07-02 23:20:26 +0530

    redirect fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -32,7 +32,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/students';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/students';
 
     /**
      * Create a new controller instance.
